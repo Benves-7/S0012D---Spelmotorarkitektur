48a3d52f2fa4b93434c83311bc537c9a6e74954f
Benjamin Vesterlund <benjamin_gb_W@hotmail.com>
2021-02-22 16:26:10 +0100

    added a vec4 position to make it easier to change the position.

--- src/MeshViewer/TransformComponent.h
+++ src/MeshViewer/TransformComponent.h
@@ -6,19 +6,25 @@ class TransformComponent : public BaseComponent
 {
 	OryolClassDecl(TransformComponent)
 public:
-	TransformComponent(Ptr<GameEntity> entity, mat4 transform)
+	TransformComponent(Ptr<GameEntity> entity, vec3 position)
 	{
-		this->transform = transform;
+		this->transform = translate(mat4(), position);
+		this->position = position;
 		this->gameEntity = entity;
 	}
 	void init()
 	{
-		variable variable;
-		variable.mat4 = new mat4(this->transform);
-		this->gameEntity->registerVariable("transform", variable);
+		variable transform;
+		transform.mat4 = new mat4(this->transform);
+		this->gameEntity->registerVariable("transform", transform);
+
+		variable pos;
+		pos.vec4 = new vec4(this->position.x, this->position.y, this->position.z, 1);
+		this->gameEntity->registerVariable("position", pos);
 	}
 private:
 	mat4 transform;
+	vec3 position;
 	Ptr<GameEntity> gameEntity;
 
 };
